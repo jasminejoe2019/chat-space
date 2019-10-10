0c3881fe11acf1fe45642a8e97cc192d708071d8
jasminejoe2019 <jasminejoe2019@gmail.com>
2019-10-10 12:22:04 +0900

    非同期dataの受け渡し

--- app/assets/javascripts/new_message.js
+++ app/assets/javascripts/new_message.js
@@ -1,4 +1,14 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${message.user_id}>${message.user_name}</a>
+                    ：
+                  </strong>
+                  ${message.text}
+                </p>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log(this)
@@ -11,6 +21,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      // console.log(data)
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.textbox').val('')
     })
   })
 })
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+# -binding.pry
+json.id           @message.id
+json.content      @message.content
+json.image        @message.image.url
+json.user_name    @message.user.name
