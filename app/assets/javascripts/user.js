0cc1c4da210bb4e7dc6b68d9c46492d02b959428
jasminejoe2019 <jasminejoe2019@gmail.com>
2019-10-23 10:55:20 +0900

    テキストフィールドに入力されるたびにイベントが発火するように

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,19 +1,7 @@
-$(function() {
-  $("#user-search-field").on("keyup", function() {
-    var input = $("#user-search-field").val();
-    console.log(input);
+$(function(){
 
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-　 　.done(function(users) {
-      $("#user-search-result").empty();
-      if (products.length !== 0) {
-        users.forEach(function(user){
-          appendName(user);
-        });
-    });
-  });
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
+    console.log(input)
+  })
+});
