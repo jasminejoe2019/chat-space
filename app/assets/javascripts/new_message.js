9472a6e77e8270698b85d3e99881079b68651c2c
jasminejoe2019 <jasminejoe2019@gmail.com>
2019-10-25 10:11:21 +0900

    message.js　修正

--- app/assets/javascripts/new_message.js
+++ app/assets/javascripts/new_message.js
@@ -1,49 +1,65 @@
 $(document).on('turbolinks:load ', function(){
   function buildHTML(message){
-    var html = 
- `<div class="message" id='${message.id}'>
-   <div class="upper-message">
-      <div class="upper-message__name">
-      ${message.user_name}
-      </div>
-      <div class="upper-message__date">
-      ${message.date}
-      </div>
-   </div>
-   <div class="lower-message">
-      <p class="lower-message__content">
-      ${message.content}
-      </p>
-      <img class="lower-message__image" src="
-      ${message.image}" alt="">
-   </div>
-</div>`
-    return html;
-  }
-  $('.new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.new_message')[0].reset();
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight });
-      $('.form__submit').attr('disabled',false);
-    })
-    .fail(function() {
-      alert('投稿ができませんでした');
-    })
+   if ( message.image ) {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+$('.new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+    $('form')[0].reset();
   })
-})
-
-
-
+   .fail(function(){
+     alert('error');
+   });
+   return false;
+ });
+});
